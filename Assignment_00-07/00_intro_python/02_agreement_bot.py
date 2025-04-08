58341e06460c11f4ad46a88bfb49c64174033fc1
999Maryam <mrym.99rashid@gmail.com>
2025-04-08 09:49:22 +0500

    intro_python_completed

---
+++ Assignment_00-07/00_intro_python/02_agreement_bot.py
@@ -0,0 +1,9 @@
+print("02_agreement_bot")
+
+def bot():
+  animal = str(input("what their favorite animal is? "))
+  print(f'My favorite animal is also {animal}!')
+
+if __name__ == "__main__":
+  bot()
+
---
+++ Assignment_00-07/00_intro_python/03_fehrenheit_to_celsius.py
@@ -0,0 +1,10 @@
+print("03_fahrenheit_to_celsius")
+
+def temp():
+  print("This code for converting fahrenheit to celsius")
+  fahrenheit_degree = float(input("Enter your fahrenheit degree. "))
+  celsius_degree = (fahrenheit_degree - 32) * 5.0/9.0
+  print(f'Tempereture {fahrenheit_degree} F = {celsius_degree} C')
+
+if __name__ == "__main__":
+  temp()
